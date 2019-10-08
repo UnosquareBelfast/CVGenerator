c4d0be9248cad7eac4b29ec87c253ca6c153d466
Fiona Phillips <fiona.phillips@bel-mc-190916a.ad.unosquare.com>
2019-10-08 12:03:06 +0100

    Add basic webpack config with babel.rc

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: "eval",
+  cache: true,
+  entry: "./src/index.js",
+  output: {
+    filename: "[hash].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/"
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      inject: "body"
+    })
+  ],
+  optimization: {
+    minimizer: [new UglifyJsPlugin()]
+  }
+};
