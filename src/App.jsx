ee020b90ea92bbe6091421d80c91b2e766f499c3
Fiona Phillips <fiona.phillips@bel-mc-190916a.ad.unosquare.com>
2019-10-28 11:10:47 +0000

    added global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import { hot } from 'react-hot-loader/root';
 import React from 'react';
 import Selector from './pages/Selector';
 
-const App = () => <Selector />;
+import { GlobalStyle } from './styled';
 
-export default hot(App);
+const App = () => (
+  <div>
+    <GlobalStyle />
+    <Selector />
+  </div>
+);
+
+export default App;
