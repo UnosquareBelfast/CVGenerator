19be815e7638f0d98931249e6d3cf971a70eb134
Fiona Phillips <fiona.phillips@bel-mc-190916a.ad.unosquare.com>
2019-10-28 10:07:33 +0000

    Add mock data for Select dropdown
    CG-14

---
+++ src/utilities/mock-data.js
@@ -0,0 +1,17 @@
+const mockDataForSelect = {
+  employees: [
+    { id: 1, name: 'Frodo Baggins' },
+    { id: 2, name: 'Samwise Gamgee' },
+    { id: 3, name: 'Meriadoc Brandybuck' },
+    { id: 4, name: 'Peregrin Took' },
+  ],
+  clients: [
+    { id: 1, name: 'Ice Cream Shop' },
+    { id: 2, name: 'Shoe Shop' },
+    { id: 3, name: 'Guitar Shop' },
+    { id: 4, name: 'Piano Shop' },
+  ],
+  templates: [{ id: 1, name: 'Unosquare Base' }, { id: 2, name: 'Unosquare Anonymous' }],
+};
+
+export default mockDataForSelect;
