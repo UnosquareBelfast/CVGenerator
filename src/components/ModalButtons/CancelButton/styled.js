3522665f41a4076c84508c14bb6a5d49fd113235
Fiona Phillips <fiona.phillips@bel-mc-190916a.ad.unosquare.com>
2019-12-18 10:41:53 +0000

    Some style fixes

--- src/components/ModalButtons/CancelButton/styled.js
+++ src/components/ModalButtons/CancelButton/styled.js
@@ -6,11 +6,12 @@ const StyledCancelButton = styled(Button)`
   background: transparent;
   border: ${({ theme }) => theme.colors.fontGrey} solid 2px;
   color: ${({ theme }) => theme.colors.fontGrey};
-  width: 100%;
-  margin-bottom: 0;
+  width: 90%;
+  margin: 25px 0 0 0;
 
   @media ${device.tabletS} {
-    margin-bottom: 38px;
+    margin: 38px
+    width: 100%;
   }
 `;
 
